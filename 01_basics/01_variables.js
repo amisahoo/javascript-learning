66cec8d2d5a853b9c33edd9a602099b1a36fa45c
amisahoo <32790840+amisahoo@users.noreply.github.com>
2023-12-03 21:59:28 +0530

    Added Variable Files

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail = "hitesh@gmail.com"
+var accountPassword="12345"
+accountCity = "Jaipur"
+
+console.log(accountId)
+console.table([accountEmail, accountPassword, accountCity])
