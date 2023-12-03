6a97342c10611c0a47b60135cb108c05c7be9ada
amisahoo <32790840+amisahoo@users.noreply.github.com>
2023-12-04 00:56:23 +0530

    Commit varible file

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,11 @@ const accountId = 144553
 let accountEmail = "hitesh@gmail.com"
 var accountPassword="12345"
 accountCity = "Jaipur"
+//accountCity = "Jaipur" // Should not be used
+let accountState;
 
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
 console.log(accountId)
-console.table([accountEmail, accountPassword, accountCity])
+console.table([accountEmail, accountPassword, accountCity, accountState])
