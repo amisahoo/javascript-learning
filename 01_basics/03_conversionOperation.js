e392ea7c450605714393f1333b9654a87ab736a7
amisahoo <32790840+amisahoo@users.noreply.github.com>
2023-12-09 20:34:06 +0530

    Conversion Operation file updated

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//true => 1 ; false => 0 ;
+
+let isLoggedIn = "hitesh";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+let isLoggedIn1="";
+let booleanIsLoggedIn1 = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn1);
+
+let someNumber =33;
+let stringNumber=String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
