3ff21aad729a01d46c1e6d3f68828abe96be9de1
amisahoo <32790840+amisahoo@users.noreply.github.com>
2023-12-09 19:49:02 +0530

    Data type file updated

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+
+1. Number : Range 2^53 - 1
+2. bigint
+3.string => ""
+4.boolean => true/false
+5.null => standalone value
+6.undefined
+7.symbol => used for uniqueness of components
+
+
+8.object
+*/
